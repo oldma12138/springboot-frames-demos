cc43f7c80e61b62e7caa5cda0fe35dcea5db8d7a
mxx <t.maxinxin@sino-commercial.com>
2023-03-21 16:05:13 +0800

    fix: 调整spring-security-demo，文件夹层级

--- spring-security-demos/spring-security-jwt/src/main/java/org/example/config/SecurityConfiguration.java
+++ spring-security-demos/spring-security-jwt/src/main/java/org/example/config/SecurityConfiguration.java
@@ -5,7 +5,6 @@ import org.example.config.entrypoint.AuthenticationEntryPointImpl;
 import org.example.config.filter.JwtAuthenticationFilter;
 import org.example.config.filter.JwtLoginFilter;
 import org.example.config.provider.AuthenticationProviderImpl;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
